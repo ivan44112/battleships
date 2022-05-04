2103c73ce83f3b6d57b262726d9bb7dff8b00f9a
ivan44112 <ivan.balen@ag04.com>
2022-05-04 10:40:00 +0200

    initial commit

---
+++ src/main/java/com/agency04/battleship/BattleshipApplication.java
@@ -0,0 +1,13 @@
+package com.agency04.battleship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BattleshipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BattleshipApplication.class, args);
+	}
+
+}
